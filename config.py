bef3b4d384b90c4aff964041ea5b2fce82705ff0
vxl3 <120146587+vxl3@users.noreply.github.com>
2023-02-03 15:41:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,26 +5,26 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578"))
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")
+API_ID  =  int ( getenv ( "API_ID" ، "23559116" ))
+API_HASH  =  getenv ( "API_HASH" ، "ac878ed19ca558e9a23e891098e328af" )
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 SESSION_NAME = getenv("SESSION_NAME", "")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9")
+OWNER_USERNAME  =  getenv ( "OWNER_USERNAME" ، "OI_I6" )
 ALIVE_NAME = getenv("ALIVE_NAME", "sonng")
 BOT_USERNAME = getenv("BOT_USERNAME", "")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/RrRRR")
+UPSTREAM_REPO  =  getenv ( "UPSTREAM_REPO" ، "https://github.com/vxl3/MyBot" )
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444")
+GROUP_SUPPORT  =  getenv ( "GROUP_SUPPORT" ، "MUSICIQD" )
+UPDATES_CHANNEL  =  getenv ( "UPDATES_CHANNEL" ، "I404P" )
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+OWNER_ID  =  list ( map ( int ، getenv ( "OWNER_ID" ، "5179721996" ). split ()))
+SUDO_USERS  =  قائمة ( خريطة ( int ، getenv ( "SUDO_USERS" ، "5179721996" ). split ()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
