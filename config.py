bcecb5b5e7d4b5c92e7828ccb4fdd90b8cbf4868
D3NE <122222544+D3NE@users.noreply.github.com>
2023-02-13 04:57:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,31 +5,30 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID  =  int ( getenv ( "API_ID" ، "23559116" ))
-API_HASH  =  getenv ( "API_HASH" ، "ac878ed19ca558e9a23e891098e328af" )
-BOT_TOKEN = getenv("BOT_TOKEN", "5783978621:AAHJoLEsec64khzOwXH6XdDy6XoT5miuFTo")
-SESSION_NAME = getenv("SESSION_NAME", "AgBSLjNibN9S2HnQstcyD-YtJqKgnN-yCx3JIzsw29IkPgWhzogVRKDCpvuXR1gMdIf9Iltxb5sFIvAeAkp_iE7fR3vvHneN9bSa0UzvyDs2S3AhCE53PSgRG7lcQIM3ZxqOxGHCFdAX4zC5KJbNgExe-3Q8L3gWmIsi9roQ1Djj-uZlMRDItP2wAprog1oQQdau2-TsyoJrstEQnHVm8sbBilyZh-nJ7GFjdNDzz5-4gC9qXCD46OlHntbaE2JxaxXaDSgxIUOPIlzIe8BW3R4C0TQip7Nwv6Ys3v0XJL0dO5ak6T4kWQzfU-BjsVETDF3pCT3Ociu5XdLMu-RD3PuuAAAAATS8SQwA")
-
+API_ID  =  int ( getenv ( "API_ID" ، "28407968" ))
+API_HASH  =  getenv ( "API_HASH" ، "422126161e78bfd7ab9ffa28a15a23f3" )
+BOT_TOKEN = getenv("BOT_TOKEN", "6163660815:AAEnVDRJuLF3CYenahT8Yjklp8s2vb4YRHw")
+SESSION_NAME = getenv("SESSION_NAME", "AgBjEzA6rDp9TO3dstx2z0fOWu5WyFtMO-FSQvkxHQwAL5zHQRxcHe-Fk8-8DUwfQ3oe3FUKdBzw17S4gsHPcv-vSWnmMlVKebAoAJ2IbCLzCJL4jF-378OjVvIHd9isAkmRO95B5cd_A2zbBAKkpJ7TEEOZSMHBq1_WmO-ICfnFzSb9urtbVyG7m-1fVoNo-w6MoNNzzcunjRIUS-5INKlhyBdL5DZ-ydojfADRm-ohcFAj_m5f-Qopkel0J1vkSDqsSarG8HPRdDhQ4Wc5p4mME2BFUIruJkkIbm-qVvLfxxXqawAZVDtS9hGdemHi52EMEX6I70AX4ZLwkQLYhOjKAAAAAXN7xnEA")
 # mandatory vars
-OWNER_USERNAME  =  getenv ( "OWNER_USERNAME" ، "OI_I6" )
+OWNER_USERNAME  =  getenv ( "OWNER_USERNAME" ، "U33I7" )
 ALIVE_NAME = getenv("ALIVE_NAME", "sonng")
-BOT_USERNAME = getenv("BOT_USERNAME", "OOII5BOT")
-UPSTREAM_REPO  =  getenv ( "UPSTREAM_REPO" ، "https://github.com/vxl3/MyBot" )
+BOT_USERNAME = getenv("BOT_USERNAME", "DHQOBoT")
+UPSTREAM_REPO  =  getenv ( "UPSTREAM_REPO" ، "https://github.com/D3NE" )
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 GROUP_SUPPORT  =  getenv ( "GROUP_SUPPORT" ، "MUSICIQD" )
-UPDATES_CHANNEL  =  getenv ( "UPDATES_CHANNEL" ، "I404P" )
+UPDATES_CHANNEL  =  getenv ( "UPDATES_CHANNEL" ، "N3ZU1" )
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID  =  list ( map ( int ، getenv ( "OWNER_ID" ، "5179721996" ). split ()))
-SUDO_USERS  =  قائمة ( خريطة ( int ، getenv ( "SUDO_USERS" ، "5179721996" ). split ()))
+OWNER_ID  =  list ( map ( int ، getenv ( "OWNER_ID" ، "28407968" ). split ()))
+SUDO_USERS  =  قائمة ( خريطة ( int ، getenv ( "SUDO_USERS" ، "28407968" ). split ()))
 
 # image resources vars
-IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
-IMG_2 = getenv("IMG_2", "https://te.legra.ph/file/90e3b3aeb77e3e598d66d.png")
-IMG_3 = getenv("IMG_3", "https://te.legra.ph/file/d70bb6fa92728763c671f.png")
-IMG_4 = getenv("IMG_4", "https://te.legra.ph/file/430dcf25456f2bb38109f.png")
-IMG_5 = getenv("IMG_5", "https://te.legra.ph/file/cd5c96a3c7e8ae1913ef3.png")
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+IMG_1 = getenv("IMG_1","https://telegra.ph//file/490b818b5b636fcc085b8.jpg")
+IMG_2 = getenv("IMG_2", "https://telegra.ph//file/d3700c6e3a4cd2701d6a1.jpg")
+IMG_3 = getenv("IMG_3", "https://telegra.ph//file/17c9eb59f87c31aa7b224.jpg")
+IMG_4 = getenv("IMG_4", "https://te.legra.ph/file/402c519808f75bd9b1803.jpg")
+IMG_5 = getenv("IMG_5", "https://telegra.ph//file/beaf049a125e4d68a0a74.jpg")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph//file/490b818b5b636fcc085b8.jpg")
